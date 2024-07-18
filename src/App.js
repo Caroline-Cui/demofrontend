a56136a18df45c44dccec63c7b516ace811243d6
caroline-cui <carolinecui99@gmail.com>
2024-07-17 22:49:07 -0700

    frontend

--- src/App.js
+++ src/App.js
@@ -1,31 +1,20 @@
-/*
-** React CORS friendly Single Page Application - https://github.com/aws-samples/react-cors-spa 
-Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
-documentation files (the "Software"), to deal in the Software without restriction, including without limitation
-the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
-and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of
-the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
-WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
-
-import logo from './logo.svg';
-import logoS3 from './logoS3.png';
-import logoCF from './logoCloudFront.png';
-import './App.css';
-import { useFetch } from "react-async"
-
-// To be replaced by the endpoint of the API deployed through the CloudFormation Template
-const APIEndPoint = 'to be replaced with your api endpoint here'
+import {useEffect, useState} from "react";
+import {Box} from "@mui/material";
+import {fetchProfile} from "./utils/fetchProfile";
+import TextRender from "./Components/TextRender";
+import {updateProfile} from "./utils/updateProfile";
 
 function App() {
+    const [name, setName] = useState('...')
+    const [email, setEmail] = useState('...')
+    const fetchData = async () => {
+        const profile = await fetchProfile()
+        setName(profile.name)
+        setEmail(profile.email)
+    }
+    useEffect(() => {
+        fetchData()
+    }, []);
   return (
     <div className="App">
         <header className="App-header">
